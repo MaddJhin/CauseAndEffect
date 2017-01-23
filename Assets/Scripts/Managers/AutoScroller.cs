be9b7b9703d25268cc4ef945f0e735e40b77d016
Francisco Carrera <fc1159@nyu.edu>
2017-01-23 12:59:22 -0500

    Credits Added
    Odd Ballista Behavior Fixed
     - Placed Objects are now kinematic (except for sphere)

---
+++ Assets/Scripts/Managers/AutoScroller.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class AutoScroller : MonoBehaviour {
+
+    public float scrollSpeed;
+
+    private float tileSizeZ;
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+        tileSizeZ = GetComponent<RectTransform>().rect.height;
+    }
+
+    void Update()
+    {
+        float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeZ);
+        transform.position = startPosition + Vector3.up * newPosition;
+    }
+}
