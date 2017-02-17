48a59733177b5882a305afc4332c8f4341ad8931
Francisco Carrera <fc1159@nyu.edu>
2017-02-16 19:47:42 -0500

    Mobile Camera Script  - Added new script to substitute old mobile camera control

--- Assets/Scripts/Camera/MobileCamera.cs
+++ Assets/Scripts/Camera/MobileCamera.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public class MobileCamera : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public int width, height;
+
+    Vector2?[] oldTouchPositions = {
+        null,
+        null
+    };
+
+    // Use this for initialization
+    void Start ()
+    {
+        // Set screen resolution
+        Screen.SetResolution(width, height, true);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+	
+
+        // If there is no touch press
+        if (Input.touchCount == 0)
+        {
+            // Reset all counts 
+            oldTouchPositions[0] = null;
+            oldTouchPositions[1] = null;
+        }
+
+        // If there is one button press
+        
+        	
 	}
 }
